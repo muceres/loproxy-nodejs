b8d079eb2998a4b29ef95fbc5ab430447868076a
muceres <muceres+github@protonmail.com>
2018-10-10 23:21:57 +0200

    feat: proxing incoming requests

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ require('dotenv').config();
 const express = require('express');
 const util = require('util');
 const app = express();
+const request = require('request');
+
 const port = process.env.PORT || 3000;
+const baseUrl = process.env.BASE_URL || 'https://www.npmjs.com';
+
 
 app.use('/', (req, res) => {
   console.log(`request : ${util.inspect(req.url)}`);
-  res.send(util.inspect(req.url));
+  console.log(`request : ${util.inspect(req.method)}`);
+  req.pipe(request(`${baseUrl}${req.url}`)).pipe(res);
 });
 
 app.listen(port, () => console.log(`Loproxy instance is running on port : ${port}.`));
