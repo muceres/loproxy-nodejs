e826a9f4b7c0287d03ec3d0b970bdee9b651a7b1
muceres <muceres+github@protonmail.com>
2018-10-10 21:47:16 +0200

    refactor: add local 'port' as optional env variable

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
+
 const express = require('express');
-let app = express();
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.send('Hello World');
 });
-
-app.listen(3000);
+app.listen(port, () => {
+    console.log(`Loproxy instance is running on port : ${port}.`)
+})
