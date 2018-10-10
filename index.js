c9e21bec5efe17b496a102d22e3445821d2393fb
muceres <muceres+github@protonmail.com>
2018-10-10 22:13:28 +0200

    feat: catch requested url

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 
 const express = require('express');
+const util = require('util');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
+app.use('/', (req, res) => {
+  console.log(`request : ${util.inspect(req.url)}`);
+  res.send(util.inspect(req.url));
 });
-app.listen(port, () => {
-    console.log(`Loproxy instance is running on port : ${port}.`)
-})
+
+app.listen(port, () => console.log(`Loproxy instance is running on port : ${port}.`));
