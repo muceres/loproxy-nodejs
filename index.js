1a342dd490880be14f523315cfe048adc1f21b03
muceres <muceres+github@protonmail.com>
2018-10-11 15:35:22 +0200

    feat: add output log for proxied requests

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 require('dotenv').config();
 
 const express = require('express');
-const util = require('util');
 const app = express();
 const request = require('request');
+const morgan = require('morgan');
 
 const port = process.env.PORT || 3000;
 const baseUrl = process.env.BASE_URL || 'https://www.npmjs.com';
 
 
+app.use(morgan(`✔ sended from localhost ${port} to ${baseUrl}:  :method :url :status :response-time ms - :res[content-length]`));
+
 app.use('/', (req, res) => {
-  console.log(`request : ${util.inspect(req.url)}`);
-  console.log(`request : ${util.inspect(req.method)}`);
   req.pipe(request(`${baseUrl}${req.url}`)).pipe(res);
 });
 
-app.listen(port, () => console.log(`Loproxy instance is running on port : ${port}.`));
+
+
+app.listen(port, () => console.log(`▶ Loproxy instance is running on port : ${port}.`));
